25ea2eb9f8a68a52d79100697e6754648b0e85d6
grinnnnnn <grinnnnnn@gmail.com>
2018-11-21 22:34:16 +0900

    Modify main logic
    이전에는 추상화 수준이 일정하지 않아 빠른 테스트 구현이 어려웠음.
    추상화 수준을 높이고 코멘트를 더함.

---
+++ src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import model.Recipe;
+import repository.RecipeRepository;
+import viewer.CLRecipeViewer;
+import viewer.RecipeViewer;
+
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+        RecipeViewer rv = new CLRecipeViewer();
+	    String q = "Brandy with Cherry";
+
+		RecipeRepository rr = new RecipeRepository();
+
+		List<Recipe> recipes = rr.query(q);//search, query, 기타 등등
+		//검색 결과 보여주기: 초기화면
+		rv.setRecipeList(recipes);
+		//검색 결과 중 하나 선택하기
+		rv.setRecipe(recipes.get(0));
+		//해당 recipe 해당 사항을 보여주기
+		rv.show();//refresh 동작.
+
+		//필수적이지 않은 스텝은 제거.
+        /*
+        recipe.setQuantity(3);
+        String recipeList = "";
+		for(int i = 0 ; i < recipe.recipeMap.size() ; i++) {
+			recipeList += "\n"+i+" : "+recipe.recipeMap.get((1+i));
+		}
+		
+		System.out.println(recipe.getRecipeName()+ " - " +recipe.getQuantity()+" : "+ recipeList);
+		*/
+	}
+}
