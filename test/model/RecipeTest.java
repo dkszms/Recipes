30f4b83bfa456c77bae742a34138e998510f9033
grinnnnnn <grinnnnnn@gmail.com>
2018-11-21 22:49:04 +0900

    Add test tutorial
    RecipeTest as an example was added.

---
+++ test/model/RecipeTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import static org.junit.Assert.*;
+
+public class RecipeTest {
+    //RecipeRepository rr = new FileRecipeRepository("recipe file path");
+    //Recipe r = DefaultRecipeRepository.get(0);
+
+    @org.junit.Test
+    public void getRecipeMap() {
+    }
+
+    @org.junit.Test
+    public void getQuantity() {
+        assertEquals(10, 5);
+    }
+
+    @org.junit.Test
+    public void setQuantity() {
+    }
+
+    @org.junit.Test
+    public void getRecipeName() {
+    }
+}
