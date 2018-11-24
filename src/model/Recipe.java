46a2e085ef3569912d60e9f2a5a444f557a1ce20
KBShin <grinnnnnn@gmail.com>
2018-11-24 18:20:18 +0900

    Add fields to recipe
    quant, etc was added.

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -3,44 +3,68 @@ package model;
 import java.util.HashMap;
 
 public class Recipe {
-	
-	String recipeName = "";
 
-	public HashMap<Integer, String> getRecipeMap() {
-		return recipeMap;
-	}
+	private String recipeName;
+	private double quantity;
+	private String simpleDesc;
+	private String image;
+	private RecipeDirections directions;
+	private String ingredients;
 
-	HashMap<Integer, String> recipeMap = new HashMap<>(); //hashmap은 오더가 보존되지 않음. 걍 리스트를 쓰는 게 좋을 수 있음.
-
-	
 	public Recipe(String q) {
 		recipeName = q;
-
-
-		if("Test".equals(q)) {
-			recipeMap.put(1, "ü�� �ľ�");
-			recipeMap.put(2, "�� �ο�");
-			recipeMap.put(3, "����");
-			recipeMap.put(4, "����");
-		}else {
-			recipeMap.put(1, "�� �ο�");
-			recipeMap.put(2, "��� �־�");
-			recipeMap.put(3, "����");
-			recipeMap.put(4, "�Ծ�");
-	
-		}
-	}
-	int quantity = 1;
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+		quantity = 10.0;
+		simpleDesc = "cherry + brandy + sugar";
+		image = "This is an image";
+		directions = RecipeDirections.getMockDirections();
+		ingredients = "cherry: 5 ea\nbrandy: 10 bottles\nsugar 1kg";
 	}
 
 	public String getRecipeName() {
 		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getSimpleDesc() {
+		return simpleDesc;
+	}
+
+	public void setSimpleDesc(String simpleDesc) {
+		this.simpleDesc = simpleDesc;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public RecipeDirections getDirections() {
+		return this.directions;
+	}
+
+	public void setDirections(RecipeDirections directions) {
+		this.directions = directions;
+	}
+
+	public String getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(String ingredients) {
+		this.ingredients = ingredients;
 	}
 }
