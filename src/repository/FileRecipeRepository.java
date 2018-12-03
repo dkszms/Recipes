8e74d49c869ce11083e238c593118acc98bed699
Ahn <agy1234@naver.com>
2018-12-03 22:38:10 +0900

    FileRecipeRepository 수정
    query 메소드 추가 (검색을 통한 Recipe 객체 추출)
    엑셀 파일만 사용하므로엑셀 확장자 구분 메소드 추가함맨~

--- src/repository/FileRecipeRepository.java
+++ src/repository/FileRecipeRepository.java
@@ -4,47 +4,61 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import model.Description;
-import model.DirectionElement;
-import model.Ingredient;
 import model.Recipe;
-import model.RecipeDirection;
+import util.IteratingExcelRecipeReader;
 
 /**
  * TODO: Repository로 재작성.
  */
 public class FileRecipeRepository {
-	//1. initialize 
 	
+	static List<Recipe> recipeList = new ArrayList<>();
 	
+	//1. initialize
 	public FileRecipeRepository() throws InvalidFormatException, IOException {
 		// 파일을 읽어온다.
-		File file = new File ("E:\\Git\\GIT Project\\Recipes\\경레시피.xlsx");
+		File file = new File ("resource/recipe_test.xlsx");
 		
 		XSSFWorkbook wb = new XSSFWorkbook(file);
 		
 		Sheet sheet = wb.getSheetAt(0);
 		
-		List<Recipe> recipeList = new ArrayList<>();
-		// POi를 사용해서 1row를 받아온다.
-		for(Row row : sheet) {
-			//row의 Cell 1,2,3,을 name, qty, simpledesc로 정의
-			//4번째 이상의 cell은 정규식을 통해 나눈 후 Direction 생성
-//			Recipe recipe = makeRecipe(row);
-//			recipeList.add(recipe);
+		if(checkFileExtention(file)) {
+			IteratingExcelRecipeReader iter = new IteratingExcelRecipeReader(file); // 엑셀 파일을 조회하여 iterater를 생성한다.
+	        for(Recipe recipe : iter) {
+	            recipeList.add(recipe);
+	        }
+		}else {
+			System.out.println("입력 된 파일이 xlsx형식의 엑셀파일이 아닙니다.");
 		}
 		
-		//마지막에 List<Recipe>
-		//엑셀 파일을 읽어서 데이터 가져오고
-		//한 줄 -> name, qty, simpleDescription, List<RecipeDerection>
+	}
+	
+	public List<Recipe> query(String keyword) {
+		List<Recipe> resultList = new ArrayList<>();
+		for(Recipe recipe : recipeList) {
+			if(recipe.getRecipeName().contains(keyword)) {
+				resultList.add(recipe);
+			}
+		}
 		
+		return resultList;
+	}
+
+	
+	public boolean checkFileExtention(File file){
+		   String fileName = file.getName();
+		   String ext = fileName.substring(fileName.indexOf(".")+1, fileName.length());
+		   boolean isXlsx = false;
+		   
+		   if(ext.equals("xlsx")) {
+			  isXlsx = true;
+		   }
+		return isXlsx;
 	}
 }
--- test/repository/FileRecipeRepositoryTest.java
+++ test/repository/FileRecipeRepositoryTest.java
@@ -19,8 +19,8 @@ import static org.junit.Assert.*;
  * @author kbshin on 2018. 12. 1.
  */
 public class FileRecipeRepositoryTest {
-    @Test
-    public void testFileRecipeRepository() throws IOException, InvalidFormatException {
+
+    public List<Recipe> testFileRecipeRepository() throws IOException, InvalidFormatException {
         File file = new File ("resource/recipe_test.xlsx");
 
         XSSFWorkbook wb = new XSSFWorkbook(file);
@@ -31,8 +31,23 @@ public class FileRecipeRepositoryTest {
         // POi를 사용해서 1row를 받아온다.
         IteratingExcelRecipeReader iter = new IteratingExcelRecipeReader(file);
         for(Recipe recipe : iter) {
-            System.out.println(recipe);
+//            System.out.println(recipe);
             recipeList.add(recipe);
         }
+        
+        return recipeList; 
     }
+    
+    @Test
+    public void dd() throws InvalidFormatException, IOException {
+    	FileRecipeRepository frr = new FileRecipeRepository();
+    	List<Recipe> results = frr.query("브랜디에 절인 체리");
+    	
+    	for(Recipe recipe : results) {
+    		System.out.println(recipe);
+    	}
+    }
+    
 }
+
+
